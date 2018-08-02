ae79eaa4468490a5baffcd3d27e8ad2ea40014f0
Nirmal Patel <nirmal.patel@intel.com>
2018-08-01 17:46:00 -0700

    RDKCOM-996: RDKB ccsp-snmp-pa agent leaks memory while running RDKB MIB
    Reason for change: snmp-subagent has been reported leaking memory while running snmpwalk of RDKB MIBs. snmp-subagent is component comming from LLC and uses shared cache to access MIB related information while running snmpwalk. The shared cache is loaded with table, row, entry and queue while retreiving information but is not able to free allocated memory to entry and queues, thus resultigng in memory leak. This code changes will take care of freeing such memory
    Test Procedure: Repeatedly ran RDKB MIB snmpwalk from machine behind CMTS and observed heap and rss memory values for snmp_subagent.
    Unit test: snmpwalk -v2c -c private wanIP 1.3.6.1.4.1.17270 every minute for 1 hour.
    Risks: None
    (cherry picked from commit f577e57d79d108c4aab48c581a4843fdfa865ea9)
    Signed-off-by: Stephen Barrett <stephen.barrett@accenture.com>
    (cherry picked from commit 3a533e32a28048a510bbc9881c8d714b8d5924e4)
    
    Change-Id: I7c24013117f60b2e4a9e557dd4f0e4e86fad1462

--- source/SnmpPlugin/ccsp_mib_definitions.h
+++ source/SnmpPlugin/ccsp_mib_definitions.h
@@ -116,6 +116,8 @@
 #define  CCSP_MIB_LIMIT_MAX         2
 #define  CCSP_MIB_LIMIT_BOTH        3
 
+typedef void (* CCSP_CLEAN_MIB_VAL_QUEUE_FUN_PTR)(void *);
+
 /* Counter64 defintion */
 typedef  struct
 _CCSP_U64
@@ -298,6 +300,7 @@ CCSP_MIB_VALUE,  *PCCSP_MIB_VALUE;
 typedef  struct
 _CCSP_TABLE_ENTRY
 {
+	CCSP_CLEAN_MIB_VAL_QUEUE_FUN_PTR CleanMibValueQueueFunctionPtr;
 	CCSP_MIB_INDEX_VALUE            IndexValue[MAXI_MIB_INDEX_COUNT];
 	ULONG							IndexCount;
 	QUEUE_HEADER                    MibValueQueue;
